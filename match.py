c4b03d64b3c011fbc63416bc88a6dd5db39f1151
Rodrigo Baravalle <rbaravalle@gmail.com>
2017-01-30 14:12:55 -0300

    Added variance to the rest of parameters

--- match.py
+++ match.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 min_distance = 100000000
 min_call = ""
 min_synth_spectrum = np.zeros(21)
-real_spectrum = np.zeros(21)
+real_spectrum = []
 
 max_iterations = 20000
 min_it = max_iterations - 10000
@@ -18,6 +18,21 @@ max_particles = 12000
 min_part = max_particles - 4000
 max_part = max_particles + 4000
 step_part = 1000
+
+max_size = 400
+min_size = max_size - 380
+max_size = max_size + 400
+step_size = 50
+
+mid_rand = 0.035
+min_rand = mid_rand - 0.02
+max_rand = mid_rand + 0.05
+step_rand = 0.01
+
+mid_rand_z = 0.15
+min_rand_z = mid_rand_z - 0.1
+max_rand_z = mid_rand_z + 0.2
+step_rand_z = 0.1
 
 def make_graphics(fds_real, fds_synth, gen_call):
     fig = plt.figure()
@@ -32,27 +47,30 @@ print "Starting for..."
 
 for it in range(min_it, max_it, step_it):
     for part in range(min_part, max_part, step_part):
-        args_call = str(it) + " " + str(part) + " 400 0.035 0.15"
-        generation_call = "./porous_generation/porous_generate " + args_call
+        for size in range(min_size, max_size, step_size):
+            for rand in np.arange(min_rand, max_rand, step_rand):
+                for rand_z in np.arange(min_rand_z, max_rand_z, step_rand_z):
+                    args_call = str(it) + " " + str(part) + " " + str(size) + " " + str(rand) + " " + str(rand_z)
+                    generation_call = "./porous_generation/porous_generate " + args_call
 
-        print ""
-        os.system(generation_call)
-        distance, synth_spectrum, real_spec = test_comparison.compare()
-        print "Distance real - synthetic : ", distance
+                    print ""
+                    os.system(generation_call)
+                    distance, synth_spectrum, real_spec = test_comparison.compare(real_spectrum)
+                    print "Distance real - synthetic : ", distance
 
-        if(distance < min_distance):
-            min_distance = distance
-            min_spectrum = synth_spectrum
-            real_spectrum = real_spec
-            min_call = "Args: " + args_call + ", " + str(min_distance)
+                    if(distance < min_distance):
+                        min_distance = distance
+                        min_spectrum = synth_spectrum
+                        real_spectrum = real_spec
+                        min_call = "Args: " + args_call + ", " + str(min_distance)
 
-            make_graphics(real_spectrum, min_spectrum, min_call)
+                        make_graphics(real_spectrum, min_spectrum, min_call)
 
-        print "CURRENT MINIMUM:"
-        print min_distance
-        print min_spectrum
-        print min_call
-        print ""
+                    print "CURRENT MINIMUM:"
+                    print min_distance
+                    print min_spectrum
+                    print min_call
+                    print ""
 
 
 
